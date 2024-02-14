4fb1cac03ae8398409c165eb3633f5cd1e7819dc
mohankanaparthi <158627396+mohankanaparthi@users.noreply.github.com>
2024-02-14 17:52:31 -0500

    Update find_natural_whole_test.py

--- integration/find_natural_whole_test.py
+++ integration/find_natural_whole_test.py
@@ -1,1 +1,9 @@
 
+import natural_whole_num
+
+
+def test_check_natural_whole(mynum):
+  testNumber = 6
+  result = numCheck.NumberCheck(testNumber)
+  
+  assert result == -3, "The number is not whole number"
