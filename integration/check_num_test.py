2d6cf6e5f0bf2a4c7bbef6cac278a59c296300df
mohankanaparthi <158627396+mohankanaparthi@users.noreply.github.com>
2024-02-14 18:03:16 -0500

    Update check_num_test.py

--- integration/check_num_test.py
+++ integration/check_num_test.py
@@ -5,4 +5,4 @@ def test_check_the_num():
   testNumber = 3
   result = numCheck.NumberCheck(testNumber)
   
-  assert result == Positive, "The number is not a positive number"
+  assert result == "Positive", "The number is not a positive number"
