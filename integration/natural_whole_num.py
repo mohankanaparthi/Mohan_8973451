7784b89d8b6b8d655990283f4592b63e327ab2cc
mohankanaparthi <158627396+mohankanaparthi@users.noreply.github.com>
2024-02-14 17:49:53 -0500

    Create natural_whole_num.py

---
+++ integration/natural_whole_num.py
@@ -0,0 +1,8 @@
+def check_number_type(number):
+ if number > 0 and number % 1 == 0:
+     return "Natural number"
+ elif number >= 0 and number % 1 == 0:
+     return "Whole number"
+ else:
+     return "Neither natural nor whole number"
+   
