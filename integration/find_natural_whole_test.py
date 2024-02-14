8475a02e04b35ae32aa52096151270ed6625cefb
mohankanaparthi <158627396+mohankanaparthi@users.noreply.github.com>
2024-02-14 17:54:23 -0500

    Update find_natural_whole_test.py

--- integration/find_natural_whole_test.py
+++ integration/find_natural_whole_test.py
@@ -3,7 +3,7 @@ import natural_whole_num
 
 
 def test_check_natural_whole(mynum):
-  testNumber = 6
+  testNumber = -2
   result = check_number_type.NumberCheck(testNumber)
   
   assert result == -3, "The number is not whole number"
